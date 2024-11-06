b1ac2e9496299ad0c04d0b91ff067eef6ca56686
Subi pra Nuvem <subipranuvembr@gmail.com>
2024-11-05 21:15:07 -0300

    Desabilitando as configurações de segurança para retornar todo tipo de conteúdo

--- ai/gemini.py
+++ ai/gemini.py
@@ -1,17 +1,28 @@
 import google.generativeai as genai
 import os
 from ai.env_var_keys import EnvironmentVarKey
+from google.generativeai.types import HarmCategory, HarmBlockThreshold
 
 
 class GeminiQuizSolver:
     def __init__(self):
-        genai.configure(api_key=os.environ[EnvironmentVarKey.GEMINI_API_KEY])
+        genai.configure(
+            api_key=os.environ[EnvironmentVarKey.GEMINI_API_KEY],
+        )
         model = "gemini-1.5-flash"
         env_model = os.environ[EnvironmentVarKey.GEMINI_MODEL]
         if env_model != "":
             model = env_model
         print(f"ai model loaded: {model}")
         self._model = genai.GenerativeModel(model)
+        # Config to deisable safety settings
+        # Check documentation here: https://ai.google.dev/gemini-api/docs/safety-settings?hl=pt-br#code-examples
+        self._safety_settings = {
+            HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+            HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
+        }
 
     def solve_quiz(self, question_and_alternatives: str) -> int:
         res = self._model.generate_content(
@@ -22,7 +33,8 @@ class GeminiQuizSolver:
             Remember only respond with an integer between 1 and 4 that corresponds to the answer.
             The question an the alternatives is:
             {question_and_alternatives}
-            """
+            """,
+            safety_settings=self._safety_settings,
         )
         guess = int(res.text)
         if guess > 4 or guess < 0:
